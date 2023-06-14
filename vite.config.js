1a15ad0a4a8408657746ac7311082b2f94565f21
Neelanjan Mukherji <neelanjanmukherji@gmail.com>
2023-06-14 23:46:26 +0530

    base added

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/AURA/',
+  base: '/AURA-website/',
   server: {
     watch: {
       usePolling: true,

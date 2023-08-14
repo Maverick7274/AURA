2ce2fd271df075eddf7d45a2b11b0ceee832b04e
Neelanjan Mukherji <neelanjanmukherji@gmail.com>
2023-08-15 03:33:58 +0530

    More Bunch of stuff added

--- src/Components/PastEvents/PastYearEvents.jsx
+++ src/Components/PastEvents/PastYearEvents.jsx
@@ -24,9 +24,9 @@ function PastYearEvents() {
         <div className='flex flex-wrap justify-center items-center gap-[3rem] sm:flex-row md:flex-col xl:flex-row'>
           <ButtonComponent cover="https://res.cloudinary.com/djqnbowat/image/upload/v1686746572/cover2022_u4zre2.jpg" link="/AURA-website/AURA2022Days" Heading='AURA 2022'/>
         </div>
-        {/* <div className='sticky m-[2rem] bottom-15 right-0 bg-accent rounded p-[0.9rem] text-white'> */}
+        {/* <div className='sticky m-[2rem] bottom-15 right-0 bg-accent rounded p-[0.9rem] text-white'>
         <CountdownCondensed />
-      </div>
+      </div> */}
     </div>
   )
 }
